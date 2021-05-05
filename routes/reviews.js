c513d7f67da3a675235484676a71e3a202edcd99
Enes <eneskurbetoglu1@gmail.com>
2021-05-05 23:43:16 +0300

    12

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,21 +4,6 @@ const catchAsync = require("../utils/catchAsync");
 const reviews = require("../controllers/reviews");
 const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 
-router.get("/", function (req, res) {
-  Campground.findById(req.params.id)
-    .populate({
-      path: "reviews",
-      options: { sort: { createdAt: -1 } }, // sorting the populated reviews array to show the latest first
-    })
-    .exec(function (err, campground) {
-      if (err || !campground) {
-        req.flash("error", err.message);
-        return res.redirect("back");
-      }
-      res.render("reviews/index", { campground: campground });
-    });
-});
-
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.postReview));
 
 router.delete(
@@ -29,6 +14,7 @@ router.delete(
 );
 
 router.put(
+  "/:id",
   catchAsync(reviews.editReview)
 );
 
