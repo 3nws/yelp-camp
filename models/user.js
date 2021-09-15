15a29049d98af3223f4e308baf4945bb07f2ae98
Enes <eneskurbetoglu1@gmail.com>
2021-09-15 23:09:05 +0300

    made lighthouse improvements concerning image sizes and added meta description not that it matters

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const ImageSchema = new Schema({
 });
 
 ImageSchema.virtual("thumbnail").get(function () {
-  return this.url.replace("/upload", "/upload/w_200");
+  return this.url.replace("/upload", "/upload/w_48");
 });
 
 const UserSchema = new Schema({
